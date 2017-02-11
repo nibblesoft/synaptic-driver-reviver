e46d4cc402d749937dc5aa0511577a04687ea3e0
Ivandro Ismael <ivandrofly@gmail.com>
2017-02-11 20:23:32 +0000

    add powershell version.

---
+++ Synamptic-Stop-working.ps1
@@ -0,0 +1,41 @@
+﻿
+clear
+# Fix issue with synamptic stop working after sleeping
+$syns = Get-Process syn*
+
+if($syns.Count -eq 0)
+{
+    exit
+    #Exit-PSHostProcess
+}
+
+foreach ($name in $syns) {
+    $Host.UI.WriteVerboseLine("Stoping: $name")
+    Stop-Process($name) -Force
+}
+
+$synpPath = 'C:\Program Files\Synaptics\SynTP\SynTPEnh.exe'
+#$Host.UI.WriteLine('Restarting process')
+
+if(Test-Path -LiteralPath $synpPath)
+{
+    Start-Process $synpPath -Verb runAs # start process and run as admin
+}
+
+try
+{
+    Restart-Service SynTPEnhService
+}
+catch # Todo: write out catched expection
+{
+    #$host.UI.WriteLine('something bad happenned')
+}
+
+<#
+foreach($name in $syns)
+{
+    $Host.UI.WriteVerboseLine("Starting: $name")
+    Start-Process($name)
+}#>
+
+# (Get-FileHash .\at.exe -Algorithm SHA1)
